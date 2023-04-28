ea1ee06b8d894a0385e353592f2ffc9ed5b99e23
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-04-28 17:57:38 -0300

    Update Aula11

--- LISTA_02/Aula11/src/aula11/Principal.java
+++ LISTA_02/Aula11/src/aula11/Principal.java
@@ -15,6 +15,21 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Visitante v1 = new Visitante();
+        v1.setNome("Juvenal");
+        v1.setIdade(22);
+        v1.setSexo("M");
+        Aluno a1 = new Aluno();
+        a1.setNome("Claudio");
+        a1.setMatricula(111);
+        a1.setCurso("Informática");
+        a1.setIdade(16);
+        a1.setSexo("M");
+        
+        a1.pagarMensalidade();
+        
+        System.out.println(v1);
+        
     }
     
 }
