8419b3afc03e6e9c01735ac9b320b5ec5d3d2749
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-04-29 14:49:49 -0300

    Define Restaurante Class

--- LISTA_02/Principal/src/principal/Principal.java
+++ LISTA_02/Principal/src/principal/Principal.java
@@ -10,11 +10,17 @@ public class Principal {
     
     public class Comodo implements IComodo{
         private String nome;
-
+        
+        public Comodo(){
+            this.nome = "";
+        }
+        public Comodo(String nome) {
+            this.nome = nome;
+        }
+        
         public String getNome() {
             return nome;
         }
-
         public void setNome(String nome) {
             this.nome = nome;
         }
@@ -25,6 +31,34 @@ public class Principal {
         public void calcularPagamento(){}
         @Override 
         public void calcularCompras(){}
+    }
+    
+    public class Restaurante extends Comodo{
+        private int qtdeCadeiras;
+        private int qtdeMesas;
+        
+        public Restaurante(String nome, int qtdeCadeiras, int qtdeMesas) {
+            super(nome);
+            this.qtdeCadeiras = qtdeCadeiras;
+            this.qtdeMesas = qtdeMesas;
+        }
+        public Restaurante(int qtdeCadeiras, int qtdeMesas) {
+            this.qtdeCadeiras = qtdeCadeiras;
+            this.qtdeMesas = qtdeMesas;
+        }
+
+        public int getQtdeCadeiras() {
+            return qtdeCadeiras;
+        }
+        public void setQtdeCadeiras(int qtdeCadeiras) {
+            this.qtdeCadeiras = qtdeCadeiras;
+        }
+        public int getQtdeMesas() {
+            return qtdeMesas;
+        }
+        public void setQtdeMesas(int qtdeMesas) {
+            this.qtdeMesas = qtdeMesas;
+        }
     }
     
     public void iniciar(){
