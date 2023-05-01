0f8b79bb011e60c5cfe5f2a7ab560c5dfe29e950
Filipe Augusto Parreira <filipeaugusto@alunos.utfpr.edu.br>
2023-05-01 11:46:00 -0300

    Ex03 Create and Done

--- LISTA_02/Ex03/src/ex03/Principal.java
+++ LISTA_02/Ex03/src/ex03/Principal.java
@@ -1,57 +1,83 @@
-package principal;
+package ex03;
 
 import java.util.ArrayList;
 
 public class Principal {
     
     public interface IComodo{
-        public abstract void emitirComanda(String pedido);
+        public abstract void emitirComanda();
         public abstract void calcularPagamento();
         public abstract void calcularCompras();
     }
     
-    public class Comodo implements IComodo{
+    public abstract class Comodo implements IComodo{
         private String nome;
         
         public Comodo(){
             this.nome = "";
         }
-        public Comodo(String nome) {
-            this.nome = nome;
-        }
-
-        public String getNome() {
-            return nome;
-        }
-        public void setNome(String nome) {
+        public Comodo(String nome){
             this.nome = nome;
         }
         
+        public String getNome(){
+            return this.nome;
+        }
+        
         @Override
-        public void emitirComanda(String pedido){}
+        public void emitirComanda(){};
         @Override
-        public void calcularPagamento(){}
-        @Override 
-        public void calcularCompras(){}
+        public void calcularPagamento(){};
+        @Override
+        public void calcularCompras(){};
+        
+        public abstract void adicionarPrato(String prato);
+        public abstract void removerPrato(String prato);
+        public abstract void exibirMenu();
+        public abstract void realizarLimpeza();
+        public abstract void receberPagamento(float valor);
     }
     
     public class Restaurante extends Comodo{
         private int qtdeCadeiras;
         private int qtdeMesas;
+        private Cozinha cozinha;
+        private float caixa;
 
         public Restaurante() {
+            super();
             this.qtdeCadeiras = 0;
             this.qtdeMesas = 0;
+            this.caixa = 0f;
+            this.cozinha = new Cozinha();
         }
         public Restaurante(int qtdeCadeiras, int qtdeMesas) {
             super();
             this.qtdeCadeiras = qtdeCadeiras;
             this.qtdeMesas = qtdeMesas;
+            this.cozinha = new Cozinha();
+            this.caixa = 0f;
+        }
+        public Restaurante(int qtdeCadeiras, int qtdeMesas, Cozinha cozinha) {
+            super();
+            this.qtdeCadeiras = qtdeCadeiras;
+            this.qtdeMesas = qtdeMesas;
+            this.cozinha = cozinha;
+            this.caixa = 0f;
         }
         public Restaurante(int qtdeCadeiras, int qtdeMesas, String nome) {
             super(nome);
             this.qtdeCadeiras = qtdeCadeiras;
             this.qtdeMesas = qtdeMesas;
+            this.cozinha = new Cozinha();
+            this.caixa = 0f;
+        }
+        public Restaurante(int qtdeCadeiras, int qtdeMesas, String nome, Cozinha cozinha) {
+            super(nome);
+            this.qtdeCadeiras = qtdeCadeiras;
+            this.qtdeMesas = qtdeMesas;
+            this.cozinha = cozinha;
+            this.caixa = 0f;
         }
 
         public int getQtdeCadeiras() {
@@ -68,6 +94,30 @@ public class Principal {
         }
         
         @Override
+        public void adicionarPrato(String prato){
+            this.cozinha.adicionarPrato(prato);
+        }
+        @Override
+        public void removerPrato(String prato){
+            this.cozinha.removerPrato(prato);
+        }
+        @Override
+        public void exibirMenu(){
+            this.cozinha.exibirMenu();
+        }
+        @Override
+        public void realizarLimpeza(){
+            this.cozinha.realizarLimpeza();
+        }
+        @Override 
+        public void receberPagamento(float valor){
+            this.caixa += valor;
+            System.out.println("O pagamento de R$" + valor + " foi recebido com sucesso.");
+            System.out.println("Saldo atual do caixa: R$" + this.caixa);
+            this.cozinha.sujeira += 5;
+        }
+        
+        @Override
         public String toString() {
             return "Restaurante{" + "nome=" + this.getNome() + ", qtdeCadeiras=" + this.getQtdeCadeiras() + ", qtdeMesas=" + this.getQtdeMesas() + '}';
         }
@@ -76,21 +126,26 @@ public class Principal {
     public class Cozinha extends Comodo{
         private int tiposRefeicao;
         private int qtdePanelas;
+        private ArrayList<String> pratos = new ArrayList<>();
+        private int sujeira;
 
         public Cozinha() {
             super();
             this.qtdePanelas = 0;
             this.tiposRefeicao = 0;
+            this.sujeira = 0;
         }
         public Cozinha(int tiposRefeicao, int qtdePanelas) {
             super();
             this.tiposRefeicao = tiposRefeicao;
             this.qtdePanelas = qtdePanelas;
+            this.sujeira = 0;
         }
         public Cozinha(int tiposRefeicao, int qtdePanelas, String nome) {
             super(nome);
             this.tiposRefeicao = tiposRefeicao;
             this.qtdePanelas = qtdePanelas;
+            this.sujeira = 0;
         }
         
         public int getTiposRefeicao() {
@@ -104,6 +159,55 @@ public class Principal {
         }
         public void setQtdePanelas(int qtdePanelas) {
             this.qtdePanelas = qtdePanelas;
+        }
+        public void verificarSujeira(){
+            if (this.sujeira >= 70){
+                System.out.println("Alerta!! Ambiente sujo, recomenda-se limpeza");
+            } else if (this.sujeira == 100){
+                System.out.println("O ambiente está em estado crítico, realizando limpeza");
+                this.realizarLimpeza();
+            }
+        }
+        
+        @Override
+        public void adicionarPrato(String prato){
+            pratos.add(prato);
+            System.out.println(prato + " foi adicionado com sucesso ao menu.");
+            this.sujeira += 5;
+        }
+        @Override
+        public void removerPrato(String prato){
+            int count = 0;
+            boolean removido = false;
+            for (String item : pratos){
+                if (item.equalsIgnoreCase(prato)){
+                    pratos.remove(count);
+                    System.out.println(prato + " foi removido com sucesso do menu.");
+                    removido = true;
+                }
+                count++;
+            }
+            if (!removido){
+                System.out.println(prato + " não consta no menu.");
+            }
+            this.sujeira += 5;
+        }
+        @Override
+        public void exibirMenu(){
+            System.out.println("Menu: ");
+            for (String item : pratos){
+                System.out.println("\t-" + item);
+            }
+            this.sujeira += 5;
+        }
+        @Override
+        public void realizarLimpeza(){
+            this.sujeira = 0;
+            System.out.println("Ambiente limpo.");
+        }
+        @Override
+        public void receberPagamento(float valor){
+            System.out.println("Cozinha não recebe pagamento.");
         }
         
         @Override
@@ -198,20 +302,30 @@ public class Principal {
     }
     
     public void iniciar(){
-        ArrayList<Comodo> lista = new ArrayList<>();
-        Comodo cozinha1 = new Cozinha(5, 12, "Cozinha1");
-        Comodo cozinha2 = new Cozinha(2, 10);
-        Comodo restaurante1 = new Restaurante(12, 6, "Restaurante1");
-        Comodo restaurante2 = new Restaurante(24, 12);
+        Restaurante restaurante = new Restaurante(16, 4);
+        Cozinha cozinha = new Cozinha(5, 12);
         
-        lista.add(cozinha1);
-        lista.add(restaurante1);
-        lista.add(cozinha2);
-        lista.add(restaurante2);
+        System.out.println("----------RESTAURANTE----------");
+        restaurante.adicionarPrato("Lasanha");
+        restaurante.adicionarPrato("Hamburguer");
+        restaurante.adicionarPrato("Pão na chapa");
+        restaurante.exibirMenu();
+        restaurante.removerPrato("Hamburguer");
+        restaurante.exibirMenu();
+        restaurante.receberPagamento(24.5f);
+        restaurante.receberPagamento(40f);
+        restaurante.realizarLimpeza();
         
-        for (Comodo comodo : lista){
-            System.out.println(comodo);
-        }
+        System.out.println("\n----------COZINHA----------");
+        cozinha.adicionarPrato("Lasanha");
+        cozinha.adicionarPrato("Hamburguer");
+        cozinha.adicionarPrato("Pão na chapa");
+        cozinha.exibirMenu();
+        cozinha.removerPrato("Hamburguer");
+        cozinha.exibirMenu();
+        cozinha.receberPagamento(24.5f);
+        cozinha.receberPagamento(40f);
+        cozinha.realizarLimpeza();
     }
     
     public static void main(String[] args) {
